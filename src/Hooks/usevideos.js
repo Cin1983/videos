4871415e6a2e6fb17a7d55b3e718ddb92ad99470
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-06-04 00:40:40 +0200

    Youtube API Key

--- src/Hooks/usevideos.js
+++ src/Hooks/usevideos.js
@@ -1,5 +1,5 @@
 import  {useState, useEffect} from 'react'
-import youtube from '../api/Youtube';
+import youtube from "../api/Youtube"
 
 
 
--- src/api/Youtube.js
+++ src/api/Youtube.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyCC5dKGyjYBQHo9XDrKqrw-nDxUa7SWr1Q";
+
+const KEY = `AIzaSyCC5dKGyjYBQHo9XDrKqrw-nDxUa7SWr1Q`;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
   params: {
-    key: API_KEY,
+    key: KEY,
     type: "video",
     part: "snippet",
     maxResults: 5
